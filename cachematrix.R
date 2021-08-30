be8ce6fb213258a6790dad089d6cea7e1631a7b9
keets9 <64926238+keets9@users.noreply.github.com>
2021-08-30 17:41:58 +0530

    Update cachematrix.R
    email: keerthu4998@gmail.com
    Week 3 assignment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
+#converted mean function to inv (as inverse) function that as basic function of set and get were same
+#set and get value of vector
+#set and get value of inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix())  {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) {inv <<- inverse}
+  getinv <- function() {inv}
+  list(set = set, get = get, setinv = setinv,getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+#converting mean to solve by changing "m" to "s"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting inversed matrix")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv
+  
 }
+
